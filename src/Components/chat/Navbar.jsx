5d66de9d1587201dcf2dbc36a49d9c61a1791069
valeriansword <chandrakumarxii@gmail.com>
2024-11-17 19:26:20 +0530

    added new

--- src/Components/chat/Navbar.jsx
+++ src/Components/chat/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { IoSearch } from "react-icons/io5";
 import { IoNotifications } from "react-icons/io5";
-import { Avatar, AvatarImage } from "@/components/ui/avatar"
+import { Avatar, AvatarImage } from "../ui/avatar.jsx"
 
 import { ChatState } from "../../context/ChatProvider"
 import {
